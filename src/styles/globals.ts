a8c84689cb0faff6783921713ed88ec580ea821e
vitorqueirosz <vitor.queiroz16@hotmail.com>
2021-08-14 13:18:40 -0300

    feat: add font-family

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -9,6 +9,29 @@ export const GlobalStyles: GlobalStyleComponent<
   unknown,
   DefaultTheme
 > = createGlobalStyle`
+
+  @font-face {
+    font-family: 'Ubuntu';
+    font-style: normal;
+    font-weight: 400;
+    src: local('Ubuntu Regular'),
+      url('/fonts/ubuntu-v15-latin-regular.woff2') format('woff2'),
+  }
+  @font-face {
+    font-family: 'Ubuntu';
+    font-style: normal;
+    font-weight: 500;
+    src: local('Ubuntu SemiBold'),
+      url('/fonts/ubuntu-v15-latin-500.woff2') format('woff2'),
+  }
+  @font-face {
+    font-family: 'Ubuntu';
+    font-style: normal;
+    font-weight: 700;
+    src: local('Ubuntu Bold'),
+      url('/fonts/ubuntu-v15-latin-700.woff2') format('woff2'),
+  }
+
   * {
     margin: 0;
     padding: 0;
@@ -24,6 +47,7 @@ export const GlobalStyles: GlobalStyleComponent<
 
   ${({ theme }) => css`
     body {
+      font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.md};
       background: ${theme.colors.primary};
     }
