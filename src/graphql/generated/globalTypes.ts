d290f3365d1d8186e8b81ad89acf3ae446825b91
vitorqueirosz <vitor.queiroz16@hotmail.com>
2021-09-16 23:30:08 -0300

    chore: remove globalTypes file from .gitignore

---
+++ src/graphql/generated/globalTypes.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+//==============================================================
+// START Enums and Input Objects
+//==============================================================
+
+export enum ENUM_COIN_TYPE {
+  bitcoin = "bitcoin",
+  cardano = "cardano",
+  ethereum = "ethereum",
+  others = "others",
+}
+
+//==============================================================
+// END Enums and Input Objects
+//==============================================================
